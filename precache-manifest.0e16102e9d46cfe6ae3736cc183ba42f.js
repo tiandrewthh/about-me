17bb2549821c003cabfc5fb9850e51bfa4e80724
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-02-20 13:08:09 +0000

    Deploy tiandrewthh/about-me to github.com/tiandrewthh/about-me.git:gh-pages

---
+++ precache-manifest.0e16102e9d46cfe6ae3736cc183ba42f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b44b60b20ae2c29d92f9c69a1de1a4e0",
+    "url": "/me/index.html"
+  },
+  {
+    "revision": "ce270a8903829cceef30",
+    "url": "/me/static/css/main.19393e92.chunk.css"
+  },
+  {
+    "revision": "dfc558704de45d560005",
+    "url": "/me/static/js/2.c39f3270.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/me/static/js/2.c39f3270.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ce270a8903829cceef30",
+    "url": "/me/static/js/main.5ffd2d8c.chunk.js"
+  },
+  {
+    "revision": "c377eaf0a7db0874557d",
+    "url": "/me/static/js/runtime-main.5bb5794c.js"
+  }
+]);
